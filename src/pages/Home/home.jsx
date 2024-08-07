4636e8c83738b08910de521431bd5fb73806d46e
Gabriel Ramos <vergilgfr@gmail.com>
2024-08-07 13:35:40 -0400

    Inicio do projeto com react

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Styles/style.css'
+
+const Home = () => {
+  return (
+<div class="container">
+        <h1>Portfólio em Construção</h1>
+        <div class="about">
+            <h3>Sobre o Portfólio:</h3>
+            <p>Este portfólio exibirá meus melhores trabalhos, habilidades e experiências. Estou animado para compartilhar com você!</p>
+        </div>
+        <div class="contact">
+            <p>Para qualquer dúvida ou contato, envie um e-mail para: <a href="mailto:contato@spardev.com">contato@spardev.com</a></p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
