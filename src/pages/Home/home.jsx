b241cf0e9059636ebed019fcfbb87a19d4adf221
Spardev <gabrielframos02@gmail.com>
2024-08-07 21:54:55 -0400

    teste de deploy

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import './Styles/style.css'
 
 const Home = () => {
   return (
-<div class="container">
+<div className="container">
+        <p><span role="img" aria-label="construction">🚧</span></p>
         <h1>Portfólio em Construção</h1>
-        <div class="about">
+        <div className="about">
             <h3>Sobre o Portfólio:</h3>
             <p>Este portfólio exibirá meus melhores trabalhos, habilidades e experiências. Estou animado para compartilhar com você!</p>
         </div>
-        <div class="contact">
+        <div className="contact">
             <p>Para qualquer dúvida ou contato, envie um e-mail para: <a href="mailto:contato@spardev.com">contato@spardev.com</a></p>
         </div>
     </div>
